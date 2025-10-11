1393407d3502a8912471cd2d865571dc08312b4c
Kihan15 <test.bin75@gmail.com>
2025-10-11 21:38:30 +0200

    ok

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "rg-automation-prod-d2c5b5b1"
     storage_account_name = "tfmstateprodd2c5b5b1"
     container_name       = "tfstate"
-    key                  = "new-project/terraform.tfstate"
+    key                  = "policy/terraform.tfstate"
   }
 }
