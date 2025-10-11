2bba349835b4590e4a062ffee38ccc3c1d7e9e76
Kihan15 <test.bin75@gmail.com>
2025-10-11 20:20:17 +0200

    ok

--- modules/policy_definition/outputs.tf
+++ modules/policy_definition/outputs.tf
@@ -1,7 +1,7 @@
-# Inside your policy_definition module source code##
-#output "policy_definition_id" {
+#Inside your policy_definition module source code##
+output "policy_definition_id" {
   # Change 'azurerm_policy_definition.this' to whatever resource 
   # name your module uses to define the policy.
- # value       = azurerm_policy_definition.def_policies.id
- # description = "The ID of the created Azure Policy Definition."
-#}
+ value       = azurerm_policy_definition.custom_policy.id
+ description = "The ID of the created Azure Policy Definition."
+}
