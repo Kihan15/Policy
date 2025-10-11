782eeeff7f29b381e918e14b1251e3092e70adf5
Kihan15 <test.bin75@gmail.com>
2025-10-11 17:44:32 +0200

    ok

--- main.tf
+++ main.tf
@@ -13,8 +13,7 @@ terraform {
 }
 
 provider "azurerm" {
-  features        {}
-  subscription_id = var.subscription_id
+  features {}
 }
 
 # ------------------------------------------------------------
