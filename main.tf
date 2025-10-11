fe61268b9c403d935826daa9cf4c3da3c14e5ba9
Kihan15 <test.bin75@gmail.com>
2025-10-11 19:38:53 +0200

    ok

--- main.tf
+++ main.tf
@@ -14,13 +14,13 @@ provider "azurerm" {
 
 
 resource "azurerm_resource_group" "demo" {
-  name     = "example-storage-test"
+  name     = "example-storage-test2"
   location = "West Europe"
 }
 
 ##  Demo of storage account
 resource "azurerm_storage_account" "StorageAccountDemo" {
-  name                     = "satestant000013"
+  name                     = "satestant2"
   resource_group_name      = azurerm_resource_group.demo.name
   location                 = azurerm_resource_group.demo.location
   account_tier             = "Standard"
