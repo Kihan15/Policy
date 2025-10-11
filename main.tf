a5fc472cc57f7f9d6c03b74147d7c5b4d43d4f4c
Kihan15 <test.bin75@gmail.com>
2025-10-11 17:30:11 +0200

    ok

--- main.tf
+++ main.tf
@@ -14,10 +14,7 @@ terraform {
 
 provider "azurerm" {
   features        {}
-  alias           = "target_sub"
-  use_oidc        = true
   subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
 }
 
 # ------------------------------------------------------------
