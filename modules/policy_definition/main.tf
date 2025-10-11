0da72e16d968324f7ba20cf13e3d308f651320dd
Kihan15 <test.bin75@gmail.com>
2025-10-11 15:40:04 +0200

    Add files via upload

---
+++ modules/policy_definition/main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_policy_definition" "def_policies" {
+  name         = var.policy_name
+  policy_type  = "Custom"
+  mode         = var.policy_mode
+  display_name = var.display_name
+  metadata     = var.metadata    #lookup(var.policy_definition, "metadata", {})
+  parameters   = var.parameters  #lookup(var.policy_definition, "parameters", {})
+  policy_rule  = var.policy_rule #definition, "policyRule", {})
+}
---
+++ modules/policy_definition/variables.tf
@@ -0,0 +1,47 @@
+variable "metadata" {
+  type        = string
+  description = "description"
+}
+
+variable "parameters" {
+  type        = string
+  description = "description"
+}
+
+variable "policy_rule" {
+  type        = string
+  description = "description"
+  default     = "type"
+}
+
+variable "display_name" {
+  type        = string
+  description = "description"
+}
+
+variable "policy_name" {
+  type        = string
+  description = "description"
+}
+
+variable "policy_mode" {
+  type        = string
+  description = "description"
+  default     = "Indexed"
+}
+/*
+
+mode - (Required) The policy mode that allows you to specify which resource types will be evaluated. 
+Possible values are 
+   All, 
+   Indexed, 
+   Microsoft.ContainerService.Data, 
+   Microsoft.CustomerLockbox.Data, 
+   Microsoft.DataCatalog.Data, 
+   Microsoft.KeyVault.Data, 
+   Microsoft.Kubernetes.Data, 
+   Microsoft.MachineLearningServices.Data, 
+   Microsoft.Network.Data
+   Microsoft.Synapse.Data
+
+*/
