b9a5bee4200ec8d976980d8dfa0c07a1e9e21307
Kihan15 <test.bin75@gmail.com>
2025-10-11 21:41:16 +0200

    ok

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "rg-automation-prod-d2c5b5b1"
     storage_account_name = "tfmstateprodd2c5b5b1"
-    container_name       = "tfstates"
+    container_name       = "tfstate"
     key                  = "policy/terraform.tfstate"
   }
 }
