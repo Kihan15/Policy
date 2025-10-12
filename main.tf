367af0ee78b24a55e2156c0321b0fbf7569d0c17
Kihan15 <test.bin75@gmail.com>
2025-10-13 00:07:25 +0200

    ok

--- main.tf
+++ main.tf
@@ -199,7 +199,6 @@ resource "azurerm_app_service_source_control" "github_link" {
   app_id                 = azurerm_app_service.ccoe_webapp.id
   repo_url               = "https://github.com/Kihan15/Policy.git"
   branch                 = "main"
-  manual_integration     = false
   use_mercurial          = false
 }
   
