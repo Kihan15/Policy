1e53c860ae27464153957d169c23c0b555318847
Kihan15 <test.bin75@gmail.com>
2025-10-16 14:31:47 +0200

    update

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,5 @@
   # Change 'azurerm_policy_definition.this' to whatever resource 
   # name your module uses to define the policy.
  # value       = azurerm_policy_definition.custom_policy.id
-  #description = "The ID of the created Azure Policy Definition."
+  #description = "The ID of the created Azure Policy Definition.."
 #}
